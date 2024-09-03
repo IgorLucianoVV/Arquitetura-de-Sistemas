5a6489df578c2b2eb086c39b0ed0bdf4ed8f79cd
Igor Luciano <65195691+IgorLucianoVV@users.noreply.github.com>
2024-09-03 18:15:11 -0300

    Fachada
    Criação da Fachada

---
+++ Fachada (Arquitetura de Sistemas/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+
+      GameFachada gameFachada = new GameFachada();
+      gameFachada.playGame();
+    }
+}
---
+++ Fachada (Arquitetura de Sistemas/System.java
@@ -0,0 +1,27 @@
+class InputSystem {
+
+  void handleInput() {
+    System.out.println("Tratando entrada do jogador");
+  }
+}
+
+class GraphicSystem {
+
+  void render() {
+    System.out.println("Renderizando gráficos do jogo");
+  }
+}
+
+class AudioSystem {
+
+  void playSound() {
+    System.out.println("Preparando para reprodução de sons do jogo");
+  }
+}
+
+class PlayerSystem {
+
+  void player() {
+    System.out.println("Preparando para a reprodução do personagem");
+  }
+}
